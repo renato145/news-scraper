dc0cebc37c9bd3ee207ae939459775d6d63fc8ed
Renato <renato145@hotmail.com>
2017-09-05 17:15:21 -0500

    fix

--- nscraper/__main__.py
+++ nscraper/__main__.py
@@ -9,7 +9,8 @@ from . import ComercioSource
 @click.option('--threads', default=-1)
 @click.option('--readmode', default='a', help="'a' or 'w'")
 def main(filename, suburl, tags, days, threads, readmode):
-	tags = tags.split(',')
+	if isinstanceof(tags, str):
+		tags = tags.split(',')
 	source = ComercioSource(filename, sub_url=suburl, tags=tags, n_days=days,
 						    n_threads=threads, read_mode=readmode)
 
